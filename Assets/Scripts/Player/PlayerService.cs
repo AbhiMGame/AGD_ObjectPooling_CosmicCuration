b782590c569e0ad00845c88140d00352a99138b6
Abhijeet Munje <99332106+AbhiMGame@users.noreply.github.com>
2024-11-07 11:42:13 +0530

    Update PlayerService.cs

--- Assets/Scripts/Player/PlayerService.cs
+++ Assets/Scripts/Player/PlayerService.cs
@@ -5,26 +5,18 @@ namespace CosmicCuration.Player
 {
     public class PlayerService
     {
-        private BulletPool bulletPool;
         private PlayerController playerController;
         private BulletPool bulletPool;
 
         public PlayerService(PlayerView playerViewPrefab, PlayerScriptableObject playerScriptableObject, BulletView bulletPrefab, BulletScriptableObject bulletScriptableObject)
         {
-<<<<<<< Updated upstream
-            bulletPool = new BulletPool(bulletPrefab, bulletScriptableObject);
-            playerController = new PlayerController(playerViewPrefab, playerScriptableObject, bulletPool);
-=======
             playerController = new PlayerController(playerViewPrefab, playerScriptableObject, bulletPrefab, bulletScriptableObject);
-            bulletPool=new BulletPool(bulletPrefab,bulletScriptableObject);
->>>>>>> Stashed changes
+            bulletPool=new BulletPool(bulletPrefab, bulletScriptableObject);
         }
 
         public PlayerController GetPlayerController() => playerController;
 
         public Vector3 GetPlayerPosition() => playerController.GetPlayerPosition();
-        
-        public void ReturnBulletToPool(BulletController bulletToReturn) => bulletPool.ReturnBullet(bulletToReturn);
         
     } 
 }

26e8114f195339e9447d8d952b434a35ab4a7392
Abhijeet Munje <99332106+AbhiMGame@users.noreply.github.com>
2024-11-07 11:40:07 +0530

    Update BulletPool.cs

--- Assets/Scripts/Bullets/BulletPool.cs
+++ Assets/Scripts/Bullets/BulletPool.cs
@@ -1,78 +1,25 @@
-<<<<<<< Updated upstream
+
 using System.Collections.Generic;
 
 namespace CosmicCuration.Bullets
 {
     public class BulletPool
     {
-        private BulletView bulletPrefab;
-        private BulletScriptableObject bulletSO;
-        private List<PooledBullet> pooledBullets = new List<PooledBullet>();
-
-        public BulletPool(BulletView bulletPrefab, BulletScriptableObject bulletSO)
-        {
-            this.bulletPrefab = bulletPrefab;
-            this.bulletSO = bulletSO;
-        }
-
-        public BulletController GetBullet()
-        {
-            if (pooledBullets.Count > 0)
-            {
-                PooledBullet item = pooledBullets.Find(item => !item.isUsed);
-                if (item != null)
-                {
-                    item.isUsed = true;
-                    return item.Bullet;
-                }
-            }
-            return CreateNewPooledBullet();
-        }
-
-        private BulletController CreateNewPooledBullet()
-        {
-            PooledBullet newBullet = new PooledBullet();
-            newBullet.Bullet = new BulletController(bulletPrefab, bulletSO);
-            newBullet.isUsed = true;
-            pooledBullets.Add(newBullet);
-            return newBullet.Bullet;
-        }
-
-        public void ReturnBullet(BulletController bullet)
-        {
-            PooledBullet pooledBullet = pooledBullets.Find(i => i.Bullet.Equals(bullet));
-            pooledBullet.isUsed = false;
-=======
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-namespace CosmicCuration.Bullets
-{
-    public class BulletPool 
-    {
         private BulletView bulletView;
         private BulletScriptableObject bulletScriptableObject;
         private List<PooledBullet> pooledBullets = new List<PooledBullet>();
 
-        public BulletPool(BulletView bulletView,BulletScriptableObject bulletScriptableObject)
+        public BulletPool(BulletView bulletPrefab, BulletScriptableObject bulletSO)
         {
-           this.bulletView=bulletView;
-            this.bulletScriptableObject=bulletScriptableObject;
->>>>>>> Stashed changes
+            this.bulletView = bulletPrefab;
+            this.bulletScriptableObject = bulletSO;
         }
 
         public class PooledBullet
         {
-            public BulletController Bullet;
+            public BulletController bullet;
             public bool isUsed;
-<<<<<<< Updated upstream
         }
-=======
 
-        }
-        
->>>>>>> Stashed changes
     }
 }
